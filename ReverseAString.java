b95d8ddd4d0b022b47405016fd62d69c764f086d
Kadari suresh <kadarisureshsdet@gmail.com>
2026-09-16 21:53:15 +0530

    Add Java program to reverse a string

---
+++ ReverseAString.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+
+        String str = "hello";
+
+        char[] arr = str.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        System.out.println(new String(arr));
+    }
+}
