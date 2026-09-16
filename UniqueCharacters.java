f7910427821bb889374bff81fc1ab16abcd9ca5e
Kadari suresh <kadarisureshsdet@gmail.com>
2026-09-16 23:32:27 +0530

    Add UniqueCharacters.java to find unique characters
    Implement a program to find unique characters in a string.

---
+++ UniqueCharacters.java
@@ -0,0 +1,22 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class UniqueCharacters  {
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+        String str="abcdabf";
+
+        int[] count=new int[256];
+
+        for(int i=0;i<str.length();i++){
+            count[str.charAt(i)]++;
+        }
+
+        for(int i=0;i<256;i++){
+            if(count[i]==1){
+                       System.out.println((char)i+" -"+count[i]);
+                //break;
+            }
+        }
+    }
+}
