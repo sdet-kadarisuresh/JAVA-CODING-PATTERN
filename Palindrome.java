2714554d74f7bf7defab43f97ff76d274257d4fc
Kadari suresh <kadarisureshsdet@gmail.com>
2026-09-16 21:59:30 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,30 @@
+class Main {
+    public static void main(String[] args) {
+
+        String str = "madam";
+
+        char[] arr = str.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        boolean palindrome = true;
+
+        while (left < right) {
+
+            if (arr[left] != arr[right]) {
+                palindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+        }
+
+        if (palindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
