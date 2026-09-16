06a7336d038017c5ae404a99980aa3b7549a1367
Kadari suresh <kadarisureshsdet@gmail.com>
2026-09-16 23:53:10 +0530

    Update Firstnonrepeated.java

--- Firstnonrepeated.java
+++ Firstnonrepeated.java
@@ -1,21 +1,25 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-//first non-repeated
+// First Non-Repeated Character
 
 class Firstnonrepeated {
+
     public static void main(String[] args) {
-        System.out.println("Start small. Ship something.");
-        String str="abcdabf";
 
-        int[] count=new int[256];
+        String str = "abcdabf";
 
-        for(int i=0;i<str.length();i++){
+        int[] count = new int[256];
+
+        // Count frequency
+        for (int i = 0; i < str.length(); i++) {
             count[str.charAt(i)]++;
         }
 
-        for(int i=0;i<256;i++){
-            if(count[i]==1){
-                       System.out.println((char)i+" -"+count[i]);
+        // Find first non-repeated character
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (count[ch] == 1) {
+                System.out.println("First non-repeated = " + ch);
                 break;
             }
         }
