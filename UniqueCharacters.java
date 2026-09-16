650ec1c0ba1ce46f34f9da73306c6689143f7948
Kadari suresh <kadarisureshsdet@gmail.com>
2026-09-16 23:54:47 +0530

    Refactor UniqueCharacters for clarity and efficiency
    Refactor UniqueCharacters class to improve readability and structure.

--- UniqueCharacters.java
+++ UniqueCharacters.java
@@ -1,21 +1,28 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
+// Find Unique Characters
 
-class UniqueCharacters  {
+class UniqueCharacters {
+
     public static void main(String[] args) {
-        System.out.println("Start small. Ship something.");
-        String str="abcdabf";
 
-        int[] count=new int[256];
+        String str = "abcdabf";
 
-        for(int i=0;i<str.length();i++){
+        int[] count = new int[256];
+
+        // Count frequency
+        for (int i = 0; i < str.length(); i++) {
             count[str.charAt(i)]++;
         }
 
-        for(int i=0;i<256;i++){
-            if(count[i]==1){
-                       System.out.println((char)i+" -"+count[i]);
-                //break;
+        // Find unique characters
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (count[ch] == 1) {
+                System.out.println(ch + " = " + count[ch]);
+
+                // Avoid duplicate checking/printing
+                count[ch] = 0;
             }
         }
     }
